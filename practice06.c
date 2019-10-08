2156847770ea92cfcff2ad06f5fc01e4c01e1cd4
JacksieCheung <55967451+JacksieCheung@users.noreply.github.com>
2019-10-08 23:23:19 +0800

    Add files via upload

--- practice06.c
+++ practice06.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
+
+void ary_set(int v[],int n,int val){
+	int i;
+	for(i=0;i<n;i++)
+		v[i]=val;
+}
+
 int main(){
-	puts("天\n地\n人");
+	int i;
+	int a[]={1,2,3,4,5};
+	ary_set(a,5,99);
+	for (i=0;i<5;i++)
+		printf("a[%d]=%d\n",i,a[i]);
 	return 0;
 }
