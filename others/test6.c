88952224f21e8b4eb88ab3e5c29fea40f82a5885
Jacksie <1799902904@qq.com>
2019-11-08 18:06:26 +0800

    message

---
+++ others/test6.c
@@ -0,0 +1,9 @@
+//一个中文字符占用两个字节
+#include<stdio.h>
+
+int main(){
+		char a[2];
+		scanf("%s",a);
+		printf("%s",a);
+		return 0;
+}
---
+++ others/test7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+		int n=0;
+		while (n++ <3)
+				;
+		printf("%d\n",n);
+		return 0;
+}
